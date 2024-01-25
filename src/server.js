6262ab07e31fe1b9c21b6dcdbbdae05dfcd21afb
Lukas Karberg <deaderlp@googlemail.com>
2024-01-25 13:30:17 +0100

    Add routes for info and skills

--- src/server.js
+++ src/server.js
@@ -19,6 +19,25 @@ app.get("/skills", async (req, res) => {
   const skills = await prisma.skill.findMany();
   res.json(skills);
 });
+
+app.get("/info", (req, res) => {
+  res.send("<h3>Sorry we're currently on break</h3>");
+});
+
+app.post("/skills", async (req, res) => {
+  const _skill = req.body;
+  try {
+    const skill = await addSkill(_skill);
+    if (skill !== null) {
+      res.status(201).json(skill);
+    } else {
+      res.status(400).json({ message: "an error occurred" });
+    }
+  } catch (e) {
+    res.status(400).json({ message: "an error occurred" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });

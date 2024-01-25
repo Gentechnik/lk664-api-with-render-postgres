b98bf1978cd487d7c6f217a39d00f3f166d9ef5f
Lukas Karberg <deaderlp@googlemail.com>
2024-01-25 10:29:19 +0100

    added skills in api route

--- src/server.js
+++ src/server.js
@@ -12,6 +12,24 @@ app.get("/", (req, res) => {
       `);
 });
 
+app.get("/skills", (req, res) => {
+  res.json([
+    {
+      idCode: "111",
+      name: "Test111",
+      url: "http111",
+      description: "desc111",
+    },
+    {
+      idCode: "222",
+      name: "Test222",
+      url: "http222",
+      description: "desc222",
+    },
+  ]);
+});
+3;
+
 app.listen(port, () => {
   `listening at http://localhost:${port}`;
 });
